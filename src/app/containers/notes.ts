6d2b55cbabc1441f2c290d9df9c676eccef6bbfd
gruppjo <jona_blubb@web.de>
2016-09-29 13:24:44 +0200

    module-6

--- src/app/containers/notes.ts
+++ src/app/containers/notes.ts
@@ -30,5 +30,9 @@ import { NoteCard } from '../ui';
 })
 
 export class Notes {
-  note = { title: 'new note', value: 'note here'}
+  note = {
+    title: 'new note',
+    value: 'note here',
+    color: 'lightblue'
+  }
 }
--- src/app/ui/note-card.ts
+++ src/app/ui/note-card.ts
@@ -36,8 +36,17 @@ import { Component, Input } from '@angular/core';
 
   `],
   template: `
-    <div class="note-card row shadow-1">
-      <div class="icon" (click)="onChecked()">
+    <div
+      class="note-card row shadow-1"
+      [ngStyle]="{'background-color': note.color}"
+      (mouseenter)="toggle()"
+      (mouseleave)="toggle()"
+    >
+      <div
+        class="icon"
+        *ngIf="showCheck"
+        (click)="onChecked()"
+      >
         <i class="material-icons">check</i>
       </div>
       <div class="col-xs-12 title">
@@ -51,7 +60,15 @@ import { Component, Input } from '@angular/core';
 })
 
 export class NoteCard {
-  @Input() note = { title: 'default title', value: 'default value' } // default input
+  @Input() note = { // default note
+    title: 'default title',
+    value: 'default value',
+    color: 'red'
+  }
+  private showCheck: boolean = false;
+  toggle() {
+    this.showCheck = !this.showCheck;
+  }
   onChecked() {
     console.log('clicked');
   }
