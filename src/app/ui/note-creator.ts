3e637b557b3d5f48c9c339cd0810b5bf576beb15
gruppjo <jona_blubb@web.de>
2016-09-29 14:01:07 +0200

    module 8

---
+++ src/app/ui/note-creator.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'note-creator',
+  styles: [`
+    .note-creator {
+      padding: 20px;
+      background-color: white;
+      border-radius: 3px;
+    }
+    .title {
+      font-weight: bold;
+      color: rgba(0,0,0,0.8);
+    }
+  `],
+  template: `
+  <div class="note-creator shadow-2">
+    <form class="row">
+      <pre>{{ newNote | json }}</pre>
+      <input
+        type="text"
+        name="newNoteTitle"
+        [(ngModel)]="newNote.title"
+        placeholder="Title"
+        class="col-xs-10 title"
+      >
+      <input
+        type="text"
+        name="newNoteValue"
+        [(ngModel)]="newNote.value"
+        placeholder="Take a note..."
+        class="col-xs-10"
+      >
+      <div class="actions col-xs-12 row between-xs">
+        <button
+          type="submit"
+          class="btn-light"
+         >
+          Done
+        </button>
+      </div>
+    </form>
+  </div>
+  `
+})
+
+export class NoteCreator {
+  newNote = {
+    title: '',
+    value: ''
+  }
+}
